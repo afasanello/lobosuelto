a7c3dcd99a46f8eeba8c2de193b5fefd3feb7804
Ricardo Alejandro Fasanello Guillermo <ricardo.fasanello@fing.edu.uy>
2024-11-04 13:42:57 -0300

    Domain config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://afasanello.github.io/lobosuelto"
+  base: "https://www.lobosuelto.dev"
 })
