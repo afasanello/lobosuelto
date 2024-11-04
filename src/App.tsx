753bd7ad558bcb41c0bab3669563e7cb5badf460
Ricardo Alejandro Fasanello Guillermo <ricardo.fasanello@fing.edu.uy>
2024-11-04 16:02:39 -0300

    Fixing routing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename="/lobosuelto">
+      <BrowserRouter>
         <ScrollToTop />
         <AnimatedRoutes />
       </BrowserRouter>
